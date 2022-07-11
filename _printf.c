60eea7e9459f87d9dd9838d009bf3cb210731203
MICAH ADDAH <addahmicah@gmail.com>
2022-07-11 08:20:19 -0700

    Add new line

--- _printf.c
+++ _printf.c
@@ -33,5 +33,5 @@ int _printf(const char *format, ...)
 	/*Calling parser function*/
 	printed_chars = parser(format, f_list, arg_list);
 	va_end(arg_list);
-	return (printed_chars)
+	return (printed_chars);
 }
