118624e9f7bad9ed21161e13b3935f207268e7dd
ATMgixthub <105108630+ATMgixthub@users.noreply.github.com>
2022-07-11 13:30:38 +0000

    Add struct

--- main.h
+++ main.h
@@ -1,27 +1,32 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
 
 /**
- * struct specifier - struct specifier
- * @valid: the valid char
- * @f: the functions associated
+ * struct prin - structure that contain flag and funtion
+ * @flag: character that identify funtion
+ * @fn: pointer to funtion
  */
 
-typedef struct specifier
+typedef struct prin
 {
-	char *valid;
-	int (*f)(va_list);
-} spec;
-
+	char flag;
+	char *(*fn)(va_list argu);
+} sir;
+void *_calloc(unsigned int nmemb, unsigned int size);
+int _strncat(char *buff, const char *format, int n);
+char *fntn(const char symbl, va_list);
+void *_strcat(char *dest, char *src);
+char *str_chr(va_list argu);
+char *str_str(va_list argu);
+int _strlen(char *s);
 int _printf(const char *format, ...);
-int print_c(va_list args);
-int print_s(va_list args);
-int print_d(va_list args);
-int print_i(va_list args);
-int _putchar(char c);
-int print_percent(va_list args);
-int (*get_func(char x))(va_list args);
-
+char *str_percent(va_list argu);
+char *str_int(va_list argu);
 #endif
